37a15e4f2e6336aca93eef399ea87a253ef12d38
Xcruser <48685531+Xcruser@users.noreply.github.com>
2023-07-27 03:38:52 +0200

    create Weather App

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# WeatherAppOBS
+# Weather App 🌦️
+
+![Weather App Screenshot](./img/chrome_9fPax5Jxo3.png)
+
+**Weather App** is a simple weather application that displays the current weather for various state capitals of Germany. The app fetches weather data from the OpenWeatherMap API and updates the information every 30 seconds with a new city.
+
+## How it works 🔄
+
+The Weather App is built using HTML, CSS, and JavaScript. It utilizes the OpenWeatherMap API to fetch weather data for different cities. The app displays the current weather, temperature, humidity, and wind speed for each city. The list of state capitals of German states is predefined in the JavaScript code.
+
+The app loads the weather data for the first city (in this case, "Erfurt") and then updates the display every 30 seconds with data for a new city from the predefined list.
+
+## Usage 🚀
+
+To use the Weather App, simply open the `index.html` file in a web browser. The app will automatically load weather data for different cities and update the display every 30 seconds.
+
+## Customization ✏️
+
+If desired, you can customize the list of cities in the `weather.js` file to add more state capitals or modify existing ones.
+
+## Note: Setting Up the API Key
+
+The API key must be set up in the `weather.js` file to successfully use the Weather App. Please follow the instructions below to correctly store the API key:
+
+1. **Obtain an API Key from OpenWeatherMap:**
+
+    - Visit the OpenWeatherMap website (https://openweathermap.org/) and create a free account.
+    - Log in and go to your Dashboard.
+    - Click on "API Keys" or "My API Keys" to generate your personal API key.
+    - Create a new API key and give it a name (e.g., "Weather App Key").
+
+2. **Set Up the API Key in the `weather.js` file:**
+
+    - Open the `weather.js` file in the main directory of your project.
+    - Locate the following line in the code:
+
+   ```javascript
+   let weather = {
+       apiKey: "YOUR_OPENWEATHERMAP_API_KEY",
+       // The rest of the code remains unchanged...
+   };
+
+Replace "YOUR_OPENWEATHERMAP_API_KEY" with the API key you obtained from OpenWeatherMap.
+Important:
+
+Make sure not to publish the modified code when uploading your project to GitHub to keep your API key private. Remember not to publish the file or exclude it from your repository if you've already done so.
+By following these steps, your API key will be correctly set up in the Weather App, allowing you to seamlessly retrieve weather data from OpenWeatherMap.
+
+javascript
+Copy code
+// List of German state capitals
+cities: [
+"Berlin",      // Berlin
+"Hamburg",     // Hamburg
+// Add more state capitals here...
+],
---
+++ weather.js
@@ -0,0 +1,84 @@
+let weather = {
+    apiKey: "YOUR_OPENWEATHERMAP_API_KEY", // Insert the APIKey here
+    cities: [
+        "Berlin",      // Berlin
+        "Hamburg",     // Hamburg
+        "München",     // Bayern
+        "Köln",        // Nordrhein-Westfalen
+        "Frankfurt",   // Hessen
+        "Stuttgart",   // Baden-Württemberg
+        "Düsseldorf",  // Nordrhein-Westfalen
+        "Dresden",     // Sachsen
+        "Leipzig",     // Sachsen
+        "Hannover",    // Niedersachsen
+        "Nürnberg",    // Bayern
+        "Bremen",      // Bremen
+        "Dortmund",    // Nordrhein-Westfalen
+        "Essen",       // Nordrhein-Westfalen
+        "Duisburg",    // Nordrhein-Westfalen
+        "Bochum",      // Nordrhein-Westfalen
+        "Wuppertal",   // Nordrhein-Westfalen
+        "Bonn",        // Nordrhein-Westfalen
+        "Bielefeld",   // Nordrhein-Westfalen
+        "Münster",     // Nordrhein-Westfalen
+        "Karlsruhe",   // Baden-Württemberg
+        "Mannheim",    // Baden-Württemberg
+        "Augsburg",    // Bayern
+        "Wiesbaden",   // Hessen
+        "Gelsenkirchen", // Nordrhein-Westfalen
+        "Mönchengladbach", // Nordrhein-Westfalen
+        "Braunschweig",   // Niedersachsen
+        "Chemnitz",       // Sachsen
+        "Kiel",           // Schleswig-Holstein
+        "Aachen"          // Nordrhein-Westfalen
+        // Insert other state capitals here...
+    ],
+    currentCityIndex: 0, // Current index of the city in the list
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey +
+            "&lang=de"
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("Es wurde kein Wetter gefunden.");
+                    throw new Error("Es wurde kein Wetter gefunden.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    // Function to display the weather in the HTML page
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // Update the corresponding elements in the HTML page
+        document.querySelector(".city").innerText = "Wetter in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Luftfeuchtigkeit: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Windstärke: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+
+    },
+    // Function to change the city and update the weather
+    search: function () {
+        this.fetchWeather(this.cities[this.currentCityIndex]); // Retrieve current city
+        this.currentCityIndex = (this.currentCityIndex + 1) % this.cities.length; // Go to the next city index
+    },
+};
+
+// Get weather for the first city and then update every 5 minutes
+weather.search();
+setInterval(function () {
+    weather.search();
+}, 5 * 60 * 1000); // 5 minutes converted into milliseconds
